49a9007b0f2df3b5771fcb0d6c64d303695a567f
Gaelle Tohme <122909212+Gaelle04@users.noreply.github.com>
2024-07-15 15:15:14 +0300

    gulp

--- src/script.js
+++ src/script.js
@@ -38,3 +38,29 @@ app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
 
+const gulp = require('gulp');
+const ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
+
+// Task to compile EJS to HTML
+gulp.task('ejs', () => {
+  return gulp.src('src/**/*.ejs')
+    .pipe(ejs({}, {}, { ext: '.html' }))
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest('dist'));
+});
+
+// Task to copy CSS files to dist directory
+gulp.task('copy-css', () => {
+  return gulp.src('src/**/*.css')
+    .pipe(gulp.dest('dist'));
+});
+
+// Task to copy JavaScript files to dist directory
+gulp.task('copy-js', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(gulp.dest('dist'));
+});
+
+// Default task to run all tasks
+gulp.task('default', gulp.series('ejs', 'copy-css', 'copy-js'));
