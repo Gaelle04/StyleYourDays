c156c677720c6dd10b4db8fe163c514fe50329fc
Gaelle Tohme <122909212+Gaelle04@users.noreply.github.com>
2024-07-15 15:26:32 +0300

    added some things on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const ejs = require('gulp-ejs');
 const rename = require('gulp-rename');
 
+// Task to compile EJS to HTML
 gulp.task('ejs', () => {
   return gulp.src('src/**/*.ejs')
     .pipe(ejs({}, {}, { ext: '.html' }))
@@ -9,4 +10,17 @@ gulp.task('ejs', () => {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.series('ejs'));
+// Task to copy CSS files to dist directory
+gulp.task('copy-css', () => {
+  return gulp.src('src/**/*.css')
+    .pipe(gulp.dest('dist'));
+});
+
+// Task to copy JavaScript files to dist directory
+gulp.task('copy-js', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(gulp.dest('dist'));
+});
+
+// Default task to run all tasks
+gulp.task('default', gulp.series('ejs', 'copy-css', 'copy-js'));
